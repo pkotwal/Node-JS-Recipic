8e1f17a02cab3d253eafbc386fc27f0e311e1c7e
Pratik Kotwal <pkotwal16@gmail.com>
2017-10-01 22:02:56 -0500

    Cheerio working

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+const cheerio = require('cheerio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,7 +17,18 @@ router.get('/', function(req, res, next) {
       // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       // console.log('body:', body); // Print the HTML for the Google homepage.
 
-
+      if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(body);
+      $('article.grid-col--fixed-tiles').each(function(i, element){
+        if(!($(this).hasClass('hub-card') || $(this).hasClass('video-card')) && !($(this).next().hasClass('article-card'))){
+          var a = $(this).find('h3').text();
+          if(!!a){
+            var recipe_name = a.trim()
+            console.log("Recipe "+i+". "+recipe_name);  
+          }
+        }
+      });
+    }
     });
 
     res.render('search', {'img': img});
